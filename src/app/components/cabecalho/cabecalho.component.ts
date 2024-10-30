55fc75c07681094688aa050a0ca4f413ec9b873b
marc-os89 <matquinhos1017@gmail.com>
2024-10-29 22:12:26 -0300

    Ajustando o menu hamburguer (falta acertar detalhes)

--- src/app/components/cabecalho/cabecalho.component.ts
+++ src/app/components/cabecalho/cabecalho.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cabecalho',
@@ -12,6 +12,38 @@ import { RouterLink } from '@angular/router';
   templateUrl: './cabecalho.component.html',
   styleUrl: './cabecalho.component.css'
 })
-export class CabecalhoComponent {
+export class CabecalhoComponent implements OnInit {
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        this.hideMenu();
+      }
+    });
+  }
+
+  toggleMenu(): void {
+    const menu = document.querySelector('.menu__itens') as HTMLElement;
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex'
+  }
+
+  hideMenu(): void {
+     const menu = document.querySelector('.menu__itens') as HTMLElement;
+      if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+      }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    const menu = document.querySelector('.menu__itens') as HTMLElement;
+    const width = window.innerWidth;
+    if (width > 767.98) {
+      menu.style.display = 'flex';
+    } else {
+      menu.style.display = 'none';
+    }
+  }
 }
